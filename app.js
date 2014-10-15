78f1d2aa01d538b8d3b5a9f70338f88a50202fdd
NyeinChan85 <nck1985@gmail.com>
2014-10-15 01:52:15 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+(function() {
+  var app = angular.module('profile', []);
+
+  app.controller('ListController', function(){
+    this.information = info;
+  });
+
+  app.controller('TabController', function(){
+    this.tab = 1;
+
+    this.setTab = function(newValue){
+      this.tab = newValue;
+    };
+
+    this.isSet = function(tabName){
+      return this.tab === tabName;
+    };
+  });
+
+
+  var info = [
+    {
+      header: '"When you learn to code, it opens up for you to learn many other things. :) "',
+      whoIam: "My name is Nyein Chan Kyaw. I'm a passionate software and web developer from New York City. I love coding, playing guitars and listening to folk songs!!!",
+      job:'vDiscovery (08/2012 - present), G & G LLC (11/2006- 06/2009)',
+      education: 'Queens Collge(2014)', 
+      projects: "will be updated soon",
+      contacts: "@nck85"
+      
+      
+      
+    }
+  ];
+})();
